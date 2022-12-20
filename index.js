bd0665110bfed5478a0406a5340b70e204ac00b6
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2022-12-19 20:48:11 -0600

    commit tree 2e27f2be6b29068b891a46d34919c01f2b1b6d33

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(cat) {
+    return cats.push(cat);
+}
+
+function destructivelyPrependCat(cat) {
+    return cats.unshift(cat);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    const newcats = [...cats, name];
+    return newcats;
+}
+
+function prependCat(name) {
+    const newcats = [name, ...cats];
+    return newcats;
+}
+
+function removeLastCat() {
+    const newcats = cats.slice(0, cats.length - 1)
+    return newcats
+}
+
+function removeFirstCat() {
+    const newcats = cats.slice(1)
+    return newcats
+}
